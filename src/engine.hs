8aabbbd738d708a4f2df0bc91251d4570b57038c
Franklin Lu <franklin@franklins-mbp.attlocal.net>
2017-11-15 13:04:10 -0600

    updated rankToBoard

--- src/engine.hs
+++ src/engine.hs
@@ -2,6 +2,7 @@ module Swai where
 
 import Data.Maybe
 import Data.Word
+import Data.Bits
 
 data Board = Board { whitePieces    :: {-# UNPACK #-} !Word64
                    , blackPieces    :: {-# UNPACK #-} !Word64
@@ -19,55 +20,55 @@ data Board = Board { whitePieces    :: {-# UNPACK #-} !Word64
 {- Piece Getters -}
 
 whiteKing :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh & ks
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh .&. ks
 
 blackKing :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl & ks
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl .&. ks
 
 whiteQueens :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh & qs
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh .&. qs
 
 blackQueens :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl & qs
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl .&. qs
 
 whiteBishops :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh & bs
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh .&. bs
 
 blackBishops :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl & bs
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl .&. bs
 
 whiteKnights :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh & ns
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh .&. ns
 
 blackKnights :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl & ns
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl .&. ns
 
 whiteRooks :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh & rs
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh .&. rs
 
 blackRooks :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl & rs
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl .&. rs
 
 whitePawns :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh & ps
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = wh .&. ps
 
 blackPawns :: Board -> Word64
-whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl & ps
+whiteKing (Board wh bl ks qs bs ns rs ps _ _ _) = bl .&. ps
 
 
--- Takes a rank (8 bit), and makes it into a board
+-- Takes a rank (8 bits), and makes it into a bitboard with 0s everywhere but that rank
 rankToBoard :: Int -> Word8 -> Word64
-rankToBoard i rank = 256 ^ (i - 1) * rank
+rankToBoard rank rankContents = shiftL rankContents (8 * (rank - 1))
 
 startingBoard :: Board
-startingBoard = Board ((rankToBoard 1 0xff) | (rankToBoard 2 0xff))
-                      ((rankToBoard 7 0xff) | (rankToBoard 8 0xff))
-                      ((rankToBoard 1 0x08) | (rankToBoard 8 0x08))
-                      ((rankToBoard 1 0x10) | (rankToBoard 8 0x10))
-                      ((rankToBoard 1 0x48) | (rankToBoard 8 0x48))
-                      ((rankToBoard 1 0x84) | (rankToBoard 8 0x84))
-                      ((rankToBoard 1 0xf1) | (rankToBoard 8 0xf1))
-                      ((rankToBoard 2 0xff) | (rankToBoard 7 0xff))
+startingBoard = Board ((rankToBoard 1 0xff) .|. (rankToBoard 2 0xff))
+                      ((rankToBoard 7 0xff) .|. (rankToBoard 8 0xff))
+                      ((rankToBoard 1 0x08) .|. (rankToBoard 8 0x08))
+                      ((rankToBoard 1 0x10) .|. (rankToBoard 8 0x10))
+                      ((rankToBoard 1 0x48) .|. (rankToBoard 8 0x48))
+                      ((rankToBoard 1 0x84) .|. (rankToBoard 8 0x84))
+                      ((rankToBoard 1 0xf1) .|. (rankToBoard 8 0xf1))
+                      ((rankToBoard 2 0xff) .|. (rankToBoard 7 0xff))
                       (True, True, True, True)
                       []
                       True
@@ -82,6 +83,6 @@ boardToString' n board =
 
 getPieceAtLocation :: Board -> Int -> Char
 getPieceAtLocation board idx =
-    if (whitePieces board) | (blackPieces board) 
+    if (whitePieces board) .|. (blackPieces board) 
 -}
 
